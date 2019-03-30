160237b6be9139c1e05d4004e3eafef910ce15e9
F嘉阳 <fjy8018@qq.com>
2019-03-30 10:12:57 +0800

    多数据源配置成功

--- src/main/java/top/fjy8018/jpadatasource/config/DataAccessConfig.java
+++ src/main/java/top/fjy8018/jpadatasource/config/DataAccessConfig.java
@@ -1,11 +1,23 @@
 package top.fjy8018.jpadatasource.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import top.fjy8018.jpadatasource.entity.backup.Order;
+import top.fjy8018.jpadatasource.entity.primary.Product;
+import top.fjy8018.jpadatasource.repository.backup.OrderRepository;
+import top.fjy8018.jpadatasource.repository.primary.ProductRepository;
+
+import javax.sql.DataSource;
 
 /**
  * @author F嘉阳
@@ -39,6 +51,50 @@ public class DataAccessConfig {
     public HikariDataSource secondDataSource() {
         return secondDataSourceProperties().initializeDataSourceBuilder()
                 .type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean firstEntityManagerFactory(
+            EntityManagerFactoryBuilder builder, @Qualifier("firstDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages(Product.class)
+                .persistenceUnit("first")
+                .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean secondEntityManagerFactory(
+            EntityManagerFactoryBuilder builder, @Qualifier("secondDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages(Order.class)
+                .persistenceUnit("second")
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager primaryTransactionManager(
+            @Qualifier("firstEntityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory.getObject());
+    }
+
+    @Bean
+    public PlatformTransactionManager backupTransactionManager(
+            @Qualifier("secondEntityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory.getObject());
+    }
+
+    @EnableJpaRepositories(basePackageClasses = ProductRepository.class,
+            entityManagerFactoryRef = "firstEntityManagerFactory", transactionManagerRef = "primaryTransactionManager")
+    @Primary
+    public class PrimaryConfiguration {
+    }
+
+    @EnableJpaRepositories(basePackageClasses = OrderRepository.class,
+            entityManagerFactoryRef = "secondEntityManagerFactory", transactionManagerRef = "backupTransactionManager")
+    public class secondConfiguration {
     }
 }
 
